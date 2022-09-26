4a910555282904a2c52ee1c99aa8fb6c406f3620
Sean <ripper.sean@yahoo.com>
2022-09-26 14:08:13 -0400

    Intro Example

---
+++ Practice Examples w Lecture/Program.cs
@@ -0,0 +1,32 @@
+﻿
+
+string restart = "Y";
+while (restart == "Y")
+{
+    Console.WriteLine("Please input the radius of a circle");
+
+    string input = Console.ReadLine();
+    double radius = double.Parse(input);
+
+    Console.WriteLine("Radius: " + radius);
+
+    double area = Math.PI * radius * radius;
+
+    Console.WriteLine("Area: " + area);
+
+    Console.WriteLine("Would you like to measure another circle? Y/N");
+
+    restart = Console.ReadLine();
+}
+
+Console.WriteLine("Goodbye!");
+
+
+
+
+
+
+
+
+
+
